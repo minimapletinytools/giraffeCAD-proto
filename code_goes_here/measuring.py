0c8bc3c11aeb69c4ebf05cd5f9ff9e93c1156b2e
minimapletinytools <minimapletinytools@gmail.com>
2026-01-28 19:31:33 -0800

    measuring stuff :D

--- code_goes_here/measuring.py
+++ code_goes_here/measuring.py
@@ -1,22 +1,48 @@
 """
 Measuring and geometric primitives for GiraffeCAD.
 
-Measuring functions follows the following pattern:
+Geometric features are things like points, lines, planes, etc. 
 
-measure(measurement, feature, timber) -> feature in global space
-mark(golbal feature, timber) -> measurement from some local feature on the timber
+Timber features are named geometric features on a timber, e.g. the centerline, the top face, etc. See `TimberFeature` enum in `timber.py` for a list of all timber features.
 
-features on timbers follow the following sign conventions:
+When marking on timbers, we want to do things like measure from a reference edge, or measure into a face. The types defined in `MarkedTimberFeature` are exactly the features that we care about when marking on timbers.
 
-- measurements from timber faces are along the normal pointing INTO the timber i.e. positive is into the face
-- measurements from timber halfplanes aligning with a timber edge 
+All measuring functions should follow the following naming convention:
+
+- measure_* : functions that take measurements relative to a (LOCAL) feature of a timber and outputs a feature in GLOBAL space
+- mark_* : functions that take a feature in GLOBAL space and outputs a measurement relative to a (LOCAL) feature of a timber
+- scribe_* : functions that take multiple measurements relative to (LOCAL) features of timbers and outputs a measurement relative to a (LOCAL) feature of a timber
+
+OR put more simply: 
+- measure_* means LOCAL to GLOBAL
+- mark_* means GLOBAL to LOCAL
+- scribe_* means LOCAL to LOCAL
+
+Using these functions, we can take measurements relative to features on one timber and mark them onto another timber. Measurements always exist in some context, and together with their context, they become colloqial ways to refer to features as it is easier to understand and work with measurements than it is to work with features directly. So measuring and marking functions are precisely used to convert between these expressions!
+
+For example, if we `my_feature = measure_into_face(mm(10), TimberFace.RIGHT, timberA)` we mean the feature that is a plane 10mm into and parallel with the right face of the timber.
+And then if we `mark_onto_face(my_feature, TimberFace.RIGHT, timberB)` we mean find the distance from the feature above to the right face of timberB
+
+Marking features assumes the features are "comparable", which in most cases means the features are parallel such that measurements can be taken in the orthognal axis. If the features are not "comparable" the functions will assert! 
+
+Some features are signed and oriented. Timber features follow the following sign conventions:
+
+- markings from timber faces are along the normal pointing INTO the timber i.e. positive is into the face
+- markings from timber halfplanes aligning with a timber edge 
     - positive X is towards the face 
     - for long faces postivie Y is (usually) in the direction of the timber, sometimes it's the opposite so watch out!
     - for end faces, use RHS rule with +Z ponting in the direction of the end
-- measurements from timber corners
+- markings from timber corners
     - TODO but also we never do this so who cares
 
-Note the Measuring feature classes are NOT used for CSG operations, they are only used for measurements and geometric calculations. 
+Features are also located (i.e. lines and planes have an "origin" point). This information is currently not used in any of these functions. Timber Features follow the following location conventions:
+
+- timber faces are located at the center of the face surface
+- timber edges are located on the bottom face of the timber
+
+A `Point` is just a `V3` and sometimes you might find yourself measuring a `Point` and simply using its contained `V3` directly! This is OK. We still wrap it in a `Point` to help ensure encapsulation of measuring and marking functions. In particular, some of these functions can take many different types of features and we want to be intentional about passing `Points` into these functions!
+
+Finally, note that the feature classes in this module should NOT be used for anything besides measuring and marking!!
 """
 
 from dataclasses import dataclass
@@ -26,7 +52,7 @@ from .timber import *
 from .timber_shavings import get_point_on_face_global, project_point_onto_face_global
 
 # Type alias for all measurable geometric features on timbers
-MeasuredTimberFeature = Union['Point', 'Line', 'Plane', 'UnsignedPlane', 'HalfPlane']
+MarkedTimberFeature = Union['Point', 'Line', 'Plane', 'UnsignedPlane', 'HalfPlane']
 
 @dataclass(frozen=True)
 class Point:
@@ -270,6 +296,14 @@ def mark_onto_face(feature: Union[UnsignedPlane, Plane, Line, Point, HalfPlane],
     This is the inverse of measure_into_face:
     If feature = measure_into_face(d, face, timber), then mark_onto_face(feature, face, timber) = d
     """
+
+    # TODO assert that UnsignedPlane/Plane/HalfPlane/Line are parallel to the face
+    if isinstance(feature, UnsignedPlane) or isinstance(feature, Plane) or isinstance(feature, HalfPlane):
+        assert are_vectors_parallel(feature.normal, timber.get_face_direction_global(face)), \
+            f"Feature must be parallel to the face. Feature {feature} is not parallel to face {face} on timber {timber}"
+    elif isinstance(feature, Line):
+        assert are_vectors_perpendicular(feature.direction, timber.get_face_direction_global(face)), \
+            f"Feature must be parallel to the face. Feature {feature} is not parallel to face {face} on timber {timber}"
 
     # Pick a point on the feature
     feature_point = get_point_on_feature(feature, timber)
